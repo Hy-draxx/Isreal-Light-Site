f6c33e652490b3ce73cd09091e508ae583e1db1e
Mohammed Ibrahim <hydrogen508@gmail.com>
2021-06-10 21:28:24 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,6 +4,18 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+  // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+  var collapsibleElem = document.querySelector('.collapsible');
+  var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+ 
+
+
   $(document).ready(function(){
           
     $("a").on('click', function(event) {
