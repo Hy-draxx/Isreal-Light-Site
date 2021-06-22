acdada22bcfe6438e2ac13ddda3b193e1467fbdf
Mohammed Ibrahim <hydrogen508@gmail.com>
2021-06-22 14:18:58 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.scrollspy');
@@ -38,22 +58,4 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   
-   
-var slide_index = 1;  
-displaySlides(slide_index);  
-function nextSlide(n) {  
-    displaySlides(slide_index += n);  
-}  
-function currentSlide(n) {  
-    displaySlides(slide_index = n);  
-}  
-function displaySlides(n) {  
-    var i;  
-    var slides = document.getElementsByClassName("showSlide");  
-    if (n > slides.length) { slide_index = 1 }  
-    if (n < 1) { slide_index = slides.length }  
-    for (i = 0; i < slides.length; i++) {  
-        slides[i].style.display = "none";  
-    }  
-    slides[slide_index - 1].style.display = "block";  
-}  
+  
